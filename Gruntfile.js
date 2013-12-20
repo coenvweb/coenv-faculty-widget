609e84c78019ad9e3c7cbca4829565938525c774
Darin Reid <darinreid@gmail.com>
2013-12-19 16:42:32 -0800

    Updating Grunt tasks, removing Compass

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,12 @@
 'use strict';
-var path = require('path');
-var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
-var folderMount = function folderMount(connect, point) {
-	return connect.static(path.resolve(point));
-};
-
-var rsyncConfig = require('./rsync-config.json');
 
 module.exports = function(grunt) {
 
 	grunt.initConfig({
 		paths: {
-			dev: '.',
-			test: 'test'
+			dev: './',
 		},
+
 		jshint: {
 			options: {
 				jshintrc: '.jshintrc'
@@ -24,8 +17,15 @@ module.exports = function(grunt) {
 				'!<%= paths.dev %>/assets/scripts/src/member.tmpl'
 			]
 		},
+
 		uglify: {
 			dist: {
+				options: {
+					sourceMap: '<%= paths.dev %>assets/scripts/maps/coenv-faculty-widget.js.map',
+					sourceMapRoot: '../src/',
+					sourceMappingURL: '../maps/main.js.map',
+					sourceMapPrefix: '3'
+				},
 				files: {
 					'<%= paths.dev %>/assets/scripts/build/coenv-faculty-widget.js': [
 						'<%= paths.dev %>/components/handlebars.js/dist/handlebars.js',
@@ -38,20 +38,7 @@ module.exports = function(grunt) {
 				}
 			}
 		},
-		compass: {
-			dist: {
-				options: {
-					sassDir: '<%= paths.dev %>/assets/styles/src',
-					cssDir: '<%= paths.dev %>/assets/styles/build',
-					imagesDir: '<%= paths.dev %>/assets/img',
-					javascriptsDir: '<%= paths.dev %>/assets/scripts/build',
-					fontsDir: '<%= paths.dev %>/assets/fonts',
-					importPath: 'components',
-					outputStyle: 'expanded',
-					relativeAssets: true
-				}
-			}
-		},
+
 		handlebars: {
 			compile: {
 				options: {
@@ -67,80 +54,89 @@ module.exports = function(grunt) {
 				}
 			}
 		},
-		watch: {
-			compass: {
-				files: ['<%= paths.dev %>/assets/styles/src/**/*.scss'],
-				tasks: ['compass']
-			},
-			srcjs: {
-				files: [
-					'<%= paths.dev %>/assets/scripts/src/**/*.js',
-					'<%= paths.dev %>/assets/scripts/src/**/*.hbs'
-				],
-				tasks: ['jshint', 'uglify']
-			},
-			livereload: {
-				files: [
-					'<%= paths.dev %>/assets/scripts/build/**/*.js',
-					'<%= paths.dev %>/assets/styles/build/*.css',
-					'<%= paths.dev %>/*.html'
-				],
-				tasks: ['livereload']
-			}
-		},
-		livereload: {
-			port: 35729 // Default livereload listening port.
-			// Must have livereload browser extension installed and working
-		},
-		connect: {
-			livereload: {
-				options: {
-					port: 9001,
-					middleware: function(connect, options) {
-						return [lrSnippet, folderMount(connect, options.base)];
-					}
+
+		sass: {
+			dist: {
+				files: {
+					'<%= paths.dev %>.tmp/assets/styles/build/coenv-faculty-widget.css': [
+						'<%= paths.dev %>assets/styles/src/coenv-faculty-widget.scss'
+					]
 				}
 			}
 		},
-		rsync: {
-			staging: {
-				src: './',
-				dest: rsyncConfig.staging.dest,
-				host: rsyncConfig.staging.host,
-				recursive: true,
-				syncDest: true
+
+		autoprefixer: {
+			dist: {
+				options: {
+					browsers: ['last 2 versions']
+				},
+				files: {
+					'<%= paths.dev %>.tmp/assets/styles/build/coenv-faculty-widget.css' : [
+						'<%= paths.dev %>.tmp/assets/styles/build/coenv-faculty-widget.css'
+					]
+				}
+			}
+		},
+
+		cssmin: {
+			dist: {
+				files: {
+					'<%= paths.dev %>assets/styles/build/coenv-faculty-widget.css' : [
+						'<%= paths.dev %>.tmp/assets/styles/build/coenv-faculty-widget.css'
+					]
+				}
+			}
+		},
+
+		watch: {
+			sass: {
+				files: ['<%= paths.dev %>assets/styles/src/**/*.scss'],
+				tasks: [ 'sass', 'autoprefixer' ]
+			},
+			css: {
+				files: ['<%= paths.dev %>.tmp/assets/styles/build/**/*.css'],
+				tasks: [ 'cssmin' ],
+				options: {
+					livereload: true
+				}
+			},
+			scripts: {
+				files: ['<%= paths.dev %>assets/scripts/src/**/*.js'],
+				tasks: [ 'jshint', 'uglify' ],
+				options: {
+					livereload: true
+				}
+			},
+			files: {
+				files: [
+					'<%= paths.dev %>**/*.{html,php}'
+				],
+				options: {
+					livereload: true
+				}
 			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-compass');
-	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.loadNpmTasks('grunt-contrib-livereload');
-	grunt.loadNpmTasks('grunt-regarde');
-	grunt.loadNpmTasks('grunt-rsync');
 	grunt.loadNpmTasks('grunt-contrib-handlebars');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.renameTask('regarde', 'watch');
-
-	grunt.registerTask('server', [
+	grunt.registerTask('dev', [
 		'default',
-		'livereload-start',
-		'connect:livereload',
 		'watch'
-	]);
-
-	grunt.registerTask('deploy', [
-		'default',
-		'rsync:staging'
 	]);
 
 	grunt.registerTask('default', [
 		'jshint',
-		'compass',
-		'handlebars',
 		'uglify',
+		'sass',
+		'autoprefixer',
+		'cssmin'
 	]);
 
 };
